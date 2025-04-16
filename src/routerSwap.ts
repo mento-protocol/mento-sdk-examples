eecfb0ffc4b540c35529705b9fbee44fc1949d5d
philbow61 <philip.raetsch@gmail.com>
2025-04-16 12:37:34 +0200

    feat: add Router example swap

--- src/routerSwap.ts
+++ src/routerSwap.ts
@@ -9,7 +9,7 @@ async function main() {
   const privateKey = process.env.PRIVATE_KEY;
   if (!privateKey) {
     console.log(
-      'Set your private key in the "privateKey" variable. before running this script.'
+      'Set your private key in the "privateKey" env variable. before running this script.'
     );
     process.exit(1);
   }
@@ -20,40 +20,51 @@ async function main() {
   const signer = new Wallet(privateKey, provider);
   const mento = await Mento.create(signer);
 
-  const celoTokenAddr = "0xF194afDf50B03e69Bd7D057c1Aa9e10c9954E4C9";
+  // swapping cUSD for cEUR using the router
+  // swapping cUSD for Celo and then Celo for cEUR
   const cUSDTokenAddr = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
-  const tokenUnits = 18; // both CELO and cUSD have 18 decimal places
+  const cEURTokenAddr = "0x10c892A6EC43a53E45D0B916B4b7D383B1b78C0F";
+  const tokenUnits = 18; // both cEUR and cUSD have 18 decimal places
+
+  // find the tradable pair path for the tokens to swap through the router
+  const tradablePair = await mento.findPairForTokens(
+    cUSDTokenAddr,
+    cEURTokenAddr
+  );
 
   const amountIn = utils.parseUnits("0.01", tokenUnits);
   const quoteAmountOut = await mento.getAmountOut(
-    celoTokenAddr,
     cUSDTokenAddr,
-    amountIn
+    cEURTokenAddr,
+    amountIn,
+    tradablePair
   );
 
   console.log(
     `~${utils.formatUnits(
       quoteAmountOut,
       tokenUnits
-    )} cUSD in exchange for 0.01 CELO`
+    )} cEUR in exchange for 0.01 cUSD`
   );
 
   console.log("Increasing trading allowance...");
   const allowanceTxObj = await mento.increaseTradingAllowance(
-    celoTokenAddr,
-    amountIn
+    cUSDTokenAddr,
+    amountIn,
+    tradablePair
   );
   const allowanceTx = await signer.sendTransaction(allowanceTxObj);
   const allowanceReceipt = await allowanceTx.wait();
   console.log("tx receipt: ", allowanceReceipt);
 
-  console.log("Swapping CELO for cUSD...");
+  console.log("Swapping cUSD for cEUR...");
   const expectedAmountOut = quoteAmountOut.mul(99).div(100); // allow 1% slippage from quote
   const swapTxObj = await mento.swapIn(
-    celoTokenAddr,
     cUSDTokenAddr,
+    cEURTokenAddr,
     amountIn,
-    expectedAmountOut
+    expectedAmountOut,
+    tradablePair
   );
   const swapTx = await signer.sendTransaction(swapTxObj);
   const swapTxReceipt = await swapTx.wait();
